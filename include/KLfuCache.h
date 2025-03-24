4130445dc8dd20c4154a49f2888df86a4fa0c86b
TOKAITEIOO <363079993@qq.com>
2025-03-24 22:31:24 +0800

    更改

--- include/KLfuCache.h
+++ include/KLfuCache.h
@@ -18,6 +18,8 @@ template<typename Key, typename Value> class KfuCache;
 
 template<typename Key, typename Value>
 class FreqList{
+private:
+    // 创建节点的基础形式
     struct Node{
         Key key;
         Value value;
@@ -25,10 +27,69 @@ class FreqList{
         std::shared_ptr<Node> pre; //上一个节点
         std::shared_ptr<Node> next; // 下一个节点
 
-        Node(): freq(1)
+        Node()
+            :freq(1)
+            ,pre(nullptr)
+            ,next(nullptr)
+        {}
+        Node(Key key, Value value)
+            :freq(1)
+            ,key(key)
+            ,value(value)
+            ,pre(nullptr)
+            ,next(nullptr)
+        {}
     };
+    using NodePtr = std::shared_ptr<Node>;
+    int freq_; //访问频率
+    NodePtr dummyHead_; //定义虚拟头结点
+    NodePtr dummytail_; //定义虚拟尾节点
+public:
+    explicit FreqList(int n)
+        : freq_(n)
+    {
+        dummyHead_ = std::make_shared<Node>(); //创建头节点
+        dummytail_ = std::make_shared<Node>();
+        dummyHead_->next = dummytail_; // 链接虚拟头结点和尾节点
+        dummytail_->pre = dummyHead_;
+    }
+    bool isEmpty() const {
+        return dummyHead_->next == dummytail_;
+    }
 
+    //添加节点的管理方法
+    void addNode(NodePtr node) {
+        if (!node || !dummyHead_ || !dummytail_) {
+            // 如果为空节点则直接返回
+            return;
+        }
+        node->pre = dummytail_->pre;
+        node->next = dummytail_->next;
+        dummytail_->pre->next = node;
+        dummytail_->pre = node;
+    }
+
+    // 移除节点的管理方法
+    void removeNode(NodePtr node) {
+        if (!node || !dummyHead_ || !dummyHead_) {
+            return;
+        }
+        if (!node->pre || !node->next) {
+            return;
+        }
+        node->pre->next = node->next;
+        node->next->pre = node->pre;
+        // 这里不delete Node是否会造成内存泄漏？
+        node->pre = nullptr;
+        node->next = nullptr;
+    }
+    // 返回第一个Node
+    NodePtr getFirstNode() const {return dummyHead_->next;}
+
+    friend class KfuCache<Key, Value>;
 };
+
+
 
 
 
